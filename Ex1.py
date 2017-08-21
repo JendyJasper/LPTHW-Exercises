84e6d02540bc38f328ca6ffd2db6d25b9e3f7d76
Jendy Jasper <jendy36@gmail.com>
2017-08-21 13:34:21 +0100

    My first exercise on python using LPTHW

---
+++ Ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing  this"
+print "This is fun."
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print 'This is chijindu iwuchukwu'
+#Do you min if it takes 100 days to get
+
