0e2834a367b6b75c7433890dec944f16d1326016
Jendy Jasper <jendy36@gmail.com>
2017-08-21 14:22:54 +0100

    The third exercise

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"
+#The decimal point makes the answer more accurate
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I wil count the eggs:"
+#  PEDMAS is used for the order of operation in python
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# Will ask the question
+print "is it true that 3 + 2 < 5 - 7?"
+# The below will return a false answer
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False"
+
+print "How about some more."
+
+print "is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
