8d9ca9afa76962497310ba51fef10634fd92a440
Jendy Jasper <jendy36@gmail.com>
2017-08-21 14:36:54 +0100

    The tenth exercise

---
+++ ex10.py
@@ -0,0 +1,18 @@
+# the backslash t(\t) means print on a new tab 
+tabby_cat = "\tI'm tabbed in."
+#the backslash n (\n) prints on a new line
+persian_cat = "I'm split \non a line."
+#The double  backslash prints only one of the backslash
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
